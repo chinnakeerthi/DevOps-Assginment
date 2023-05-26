3a9a041cf419a3b96e9c9c16449033ee2d6e67af
chinnakeerthi <keerthichinna52@gmail.com>
2023-05-26 16:07:25 +0530

    create_directory.sh*

---
+++ create_directory.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the directory name
+directory_name="my_directory"
+
+# Create the directory
+mkdir "$directory_name"
+
+# Set the permissions to 756
+chmod 756 "$directory_name"
